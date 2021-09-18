ecdf473155db40284ef52451044b4f1a4fa38cfc
Lucas <lucas.santos@wecontent.com.br>
2021-09-18 01:03:55 -0300

    removed watch, sourcemaps and nodesass in require, added new structure and added removed files generate automactly, added live-server, fs and htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,81 @@
-const gulp = require('gulp');
-const { watch } = require('gulp');
+const gulp = require('gulp'),
+      sass = require('gulp-sass')(require('sass')),
+      liveServer = require('live-server'),
+      fs = require('fs'),
+      htmlmin = require('gulp-htmlmin'),
+      rename = require('gulp-rename'),
+      minify = require('gulp-minify');
 
-const sass = require("gulp-sass")(require("node-sass"));
-const sourcemaps = require('gulp-sourcemaps');
-const rename = require('gulp-rename');
-const minify = require('gulp-minify');
 
-//objeto para minificação do CSS
+//objeto onde estão os caminhos
 const paths = {
   css: {
-    src: "assets/src/scss/**/*.scss", //onde escrevemos
+    src: "assets/src/scss/*.scss", //onde escrevemos
     dist: "assets/dist/css" // onde é renderizado
   },
+  html: {
+    src: "index.html", //onde escrevemos
+    dist: "assets/dist" // onde é renderizado
+  },
   js: {
-    src: "assets/src/js/**/*.js",
-    dist: "assets/dist/js"
+    src: "assets/src/js/*.js", //onde escrevemos
+    dist: "assets/dist/js" // onde é renderizado
   }
 }
 
-//função que minifica o css
-gulp.task("sass", gulp.series(function(){
-  return gulp.src(paths.css.src)
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' })) // usada pra comprimir o sass
-    .pipe(rename(function(path) {
-      path.basename += ".min"; // depois de comprimido adiciona o .min
-      path.extname = ".css" //  e adiciona o .css
-    }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(paths.css.dist));
-}))
+// função para pegar o file html no src e jogar minificado no dist
+gulp.task('html', function(){
+  return gulp.src(paths.html.src)
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(rename(function (path) {
+    path.basename += ".min";
+    path.extname = ".html";
+  }))
+  .pipe(gulp.dest(paths.html.dist));
+});
 
-//função que minifica o JS
-gulp.task("scripts", gulp.series(function(){
+// função para pegar o .scss no src e jogar comprimido e renomeado no dist
+gulp.task('sass', function() {  
+  return gulp.src(paths.css.src)
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(rename(function (path) {
+    path.basename += ".min";
+    path.extname = ".css";
+  }))
+  .pipe(gulp.dest(paths.css.dist));
+});
+
+// função para pegar o js no src e jogar minificado e renomeado no dist
+gulp.task("scripts", function(){
   return gulp.src(paths.js.src)
   .pipe(minify())
   .pipe(gulp.dest(paths.js.dist));
-}));
+});
 
-//função Watch que assiste as alterações
+//função que fica assistindo as outras tasks em parelelo
 gulp.task('watch', gulp.series(function(){
+  gulp.watch(paths.html.src, gulp.parallel( ["html"] ));
   gulp.watch(paths.css.src, gulp.parallel( ["sass"] ));
   gulp.watch(paths.js.src, gulp.parallel( ["scripts"] ));
 }));
 
-//função principal de tarefas
-gulp.task('default', gulp.series(['sass', "scripts"]));
+//parametros do live-server
+var params = {
+  port: 3000,
+  open: true,
+  file: paths.html.src,
+  wait: 1000,
+  logLevel: 2
+};
+
+fs.unlink('assets/dist/js/index.js', function (err){
+ try {
+   if (err) throw err;
+   liveServer.start(params);
+  } catch (error) {
+    console.log("Não foi possível encontrar o arquivo :(((")
+    liveServer.start(params);
+  }
+})
+
+console.log('Arquivo desnecessário deletado! :)))');
